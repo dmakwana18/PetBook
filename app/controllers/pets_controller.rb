eb69a103d5afc8893a7c85334ccfa8f03a38cf4c
Devang Makwana <dmakwana@uncc.edu>
2017-11-15 21:16:30 +0000

    Created Schema and Create pet (AP and DM)

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,9 +2,20 @@ class PetsController < ApplicationController
     def new
     end
     
+    def show
+        @pet = Pet.find(params[:id])
+    end
 
     def create
-        render plain: params[:pet].inspect
+        @pet = Pet.new(pet_params)
+        
+        @pet.save
+        redirect_to @pet
     end
     
+end
+
+private 
+def pet_params
+    params.require(:pet).permit(:name, :pettype)
 end
