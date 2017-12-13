5c743bf5554ca6e683c2750d32f39260fa4ee924
Devang Makwana <dmakwana@uncc.edu>
2017-12-13 17:05:54 +0000

    Added routes to the comment (AP)

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   
-  resources :pets
+  resources :pets do
+    resources :comments
+  end
   
-  root 'welcome#index'
-
+  root 'welcome#index'  
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
