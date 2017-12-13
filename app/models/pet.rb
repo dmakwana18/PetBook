f1dc1b025160412515d60b988a4e7558270d6539
Devang Makwana <dmakwana@uncc.edu>
2017-12-13 16:52:14 +0000

    Created Associations (AP)

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,16 @@
 class Pet < ApplicationRecord
-    has_many :pet
+   has_many :comments, dependent: :destroy
+   validates :name, presence: true, length: {minimum: 3}
+   validates :pet_type, presence: true
+   validates :traits, presence: true
+   validates :pet_species, presence: true
+   validates :age, presence: true, length: {maximum: 2}
+   validates :gender, presence: true, length: {maximum: 6}
+   validates :description, presence: true
+   validates :owner, presence: true
+   validates :number, presence: true
+   validates :email, presence: true
+   validates :city, presence: true
+
+
 end
